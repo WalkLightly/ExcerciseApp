bc309e2f0fa9776efc7cde1a799333391e894b83
WalkLightly <runnerknight@gmail.com>
2026-05-27 15:51:21 -0600

    Initial Commit

---
+++ Track It/Track_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Track_ItApp.swift
+//  Track It
+//
+//  Created by Michael Knight on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Track_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
