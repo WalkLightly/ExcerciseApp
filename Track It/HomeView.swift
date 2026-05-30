56429185fc6ba6cec4a8ddb19d987c5ada837419
WalkLightly <runnerknight@gmail.com>
2026-05-30 08:11:05 -0600

    Initial work for stopwatch up top

--- Track It/HomeView.swift
+++ Track It/HomeView.swift
@@ -10,9 +10,60 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         ZStack {
+            Color(.backgroundBlue)
+            VStack {
+                ZStack {
+                    VStack {
+                        
+                    }
+                    .frame(width: 345, height: 30)
+                    .background(.skyBlue)
+                    .cornerRadius(18)
+                    .offset(y: 90)
+                    HStack {
+//                        Button {
+//                        } label: {
+//                            Image(systemName: "arrow.trianglehead.clockwise")
+//                                .font(.system(size: 30))
+//                                .foregroundStyle(.gray)
+//                        }.frame(width: 60, height: 60)
+//                            .background(.accentGrey.opacity(0.8))
+//                            .clipShape(Capsule())
+                        Button {
+                        } label: {
+                            Image(systemName: "xmark")
+                                .font(.system(size: 30))
+                                .foregroundStyle(.red.opacity(0.8))
+                        }.frame(width: 60, height: 60)
+                            .background(.red.opacity(0.2))
+                            .clipShape(Capsule())
+                        HStack {
+                            Text("0 1 : 0 2 . 0 5")
+                                .font(.custom("Inder-Regular", size: 25))
+                        }
+                        .frame(width: 200, height: 70)
+                        .background(.gray)
+                        .cornerRadius(10)
+                        Button {
+                        } label: {
+                            Image(systemName: "play.fill")
+                                .font(.system(size: 30))
+                                .foregroundStyle(Color.green)
+                        }
+                        .frame(width: 60, height: 60)
+                        .background(Color.green.opacity(0.3))
+                        .clipShape(Capsule())
+                    }
+                    .frame(width: 349, height: 80)
+                    .background(.gray)
+                    .cornerRadius(12)
+                    .offset(y: 60)
+
+                }
+                Spacer()
+            }
         }
-        .background(Color.backgroundBlue)
-        .padding()
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
