3a8ca02cb54012dd952ed875f9bfb395e872b881
Michael <mknighter62@gmail.com>
2026-05-31 10:46:45 -0600

    New weight, excercise, and measurement modals added in

---
+++ Track It/NewItemViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewItemViewModel.swift
+//  Track It
+//
+//  Created by Michael Knight on 5/31/26.
+//
+
+import Foundation
---
+++ Track It/ViewModels/NewItemViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  NewItemViewModel.swift
+//  Track It
+//
+//  Created by Michael Knight on 5/31/26.
+//
+
+import Foundation
+import Observation
+//internal import Combine
+
+class NewItemViewModel {//}: ObservableObject {
+    
+}
